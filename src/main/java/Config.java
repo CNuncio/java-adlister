632b7bda978368b17d483df83e5d0f413509b8f4
Christina Nuncio <nuncio.christina01@gmail.com>
2021-11-22 01:00:44 -0600

    partials directory created

---
+++ src/main/java/Config.java
@@ -0,0 +1,16 @@
+public class Config {
+
+public String getUrl() {
+    return;
+}
+
+public String getUser() {
+    return "teeko";
+}
+
+    public String getPassword() {
+        return "password";
+    }
+
+
+}
